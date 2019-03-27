f76c1398983c724c2f61c05ede8a4414cb6c548f
fumio nonaka <fumio.nonaka@staff.gu3.co.jp>
2019-03-27 15:18:19 +0900

    React入門 02: ライブラリMarkedでMarkdownの機能を加える

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import marked from 'marked';
 
 class Comment extends Component {
+  rawMarkup() {
+    const rawMarkup = marked(this.props.children.toString());
+    return { __html: rawMarkup };
+  }
   render() {
     return (
       <div className="Comment">
         <h2 className="CommentAuthor">
         {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
